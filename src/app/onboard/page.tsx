43c82e7b41f7a032f86965e33f5eedbdf656032e
Mani061212 <Mk7307127@gmail.com>
2025-06-25 15:49:29 +0530

    update commit

--- src/app/onboard/page.tsx
+++ src/app/onboard/page.tsx
@@ -1,7 +1,6 @@
 // src/app/onboard/page.tsx
 "use client";
 
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
